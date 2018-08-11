0a7510f9a31b03bb53b588cd3c7c5039195660e8
Jordan Farkas <jordfark@gmail.com>
2018-08-11 17:41:31 +0000

    Done.

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,27 @@
 # Write your code here.
+require "pry"
+
+def dictionary 
+  
+  dictionary_hash  = {
+  "hello" => "hi", 
+  "to, two, too" => "2", 
+  "for, four" => "4",
+  "be" => "b",
+  "at" => "@",
+  "and" => "&"
+}
+
+end 
+
+def word_substituter(tweet)
+  arr = tweet.split
+  arr.each do |word|
+    # binding.pry
+    if word == dictionary.keys
+      word == dictionary.values
+      
+end 
+end 
+arr.join(' ')
+end

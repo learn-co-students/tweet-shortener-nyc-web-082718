e6b8837788f8ef22dd0cf33e87a974db1c021af6
Jordan Farkas <jordfark@gmail.com>
2018-08-12 19:32:46 +0000

    Done.

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -11,6 +11,7 @@ def dictionary
   "for" => "4",
   "four" => "4",
   "be" => "b",
+  "you" => "u",
   "at" => "@",
   "and" => "&"
 }
@@ -18,15 +19,38 @@ def dictionary
 end 
 
 def word_substituter(tweet)
-  arr = tweet.split
-  arr.map do |word|
-    # binding.pry
-    if dictionary.keys.include?(word)
-      # binding.pry
-      word = dictionary[word]
-      binding.pry
+  tweet.split.map do |word|
+    if dictionary.keys.include?(word.downcase)
+      word = dictionary[word.downcase]
+    else 
+      word
   
 end 
 end.join(' ')
-# arr.join(' ')
 end
+
+
+def bulk_tweet_shortener(tweets)
+  i = 0 
+  while i < tweets.length 
+  puts word_substituter(tweets[i])
+  i+=1 
+end
+end 
+
+def selective_tweet_shortener(tweet)
+  if tweet.length > 140 
+    word_substituter(tweet)
+  else 
+    tweet
+  end
+end 
+
+def shortened_tweet_truncator(tweet)
+  shortened_tweet = word_substituter(tweet)
+  if shortened_tweet.length > 140
+      # binding.pry
+    shortened_tweet[0...137] + "..."
+  else tweet
+end 
+end 
